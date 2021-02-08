64522126c890c29eeeb432aa5679ec1e2cfe16cd
Prasad Dhupkar <pmdhupkar@gmail.com>
2021-02-08 12:59:21 +0530

    Updated Code.

--- SDL_csharp/contents.Designer.cs
+++ SDL_csharp/contents.Designer.cs
@@ -184,7 +184,7 @@ namespace SDL_csharp
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.SlateGray;
+            this.label1.ForeColor = System.Drawing.Color.DimGray;
             this.label1.Location = new System.Drawing.Point(54, 28);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(149, 25);
